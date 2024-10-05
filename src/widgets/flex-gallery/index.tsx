6b04844003580251c0c73090c48ae3d6d94e3d4f
annadan1 <anna_s92_@mail.ru>
2024-10-05 11:25:42 +0400

    add variables and flex-gallery styles

--- src/widgets/flex-gallery/index.tsx
+++ src/widgets/flex-gallery/index.tsx
@@ -1,15 +1,17 @@
 import { type IImageData } from '@/shared/types';
 import Image from 'next/image';
+import cn from 'classnames';
+import styles from './flex-gallery.module.scss';
 
 export const FlexGallery:FCClass<{ images: IImageData[] }> = ({ images }) => {
   return (
-    <div>
+    <div className={cn(styles.container, 'container')}>
       <h1>Gallery</h1>
-      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '10px' }}>
+      <div className={styles.wrapper}>
         {images.map((image) => (
-          <div key={image.name}>
-            <Image src={image.url} alt={image.name} width={500} height={500} />
-            <p>{image.name}</p>
+          <div key={image.name} className={styles.imageWrapper}>
+            <Image src={image.url} alt={image.name} fill className={styles.image} />
+            <p className={styles.imageName}>{image.name}</p>
           </div>
         ))}
       </div>
